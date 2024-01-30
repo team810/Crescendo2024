1eb55ff2326d905e5cf7ab25ded57b45585539c1
LordScarvius <lordscarvius@gmail.com>
2024-01-30 16:11:33 -0500

    deadband's back oh yeah

--- src/main/java/frc/lib/Deadband.java
+++ src/main/java/frc/lib/Deadband.java
@@ -5,13 +5,13 @@ import edu.wpi.first.math.MathUtil;
 
 public class Deadband {
 
-	private final double deadBand; // The dead band range
+    private final double deadBand; // The dead band range
 
-	public Deadband(double deadBand) {
-		this.deadBand = deadBand;
-	}
+    public Deadband(double deadBand) {
+        this.deadBand = deadBand;
+    }
 
-	public double apply(double input) {
-		return MathUtil.applyDeadband(input,deadBand);
-	}
+    public double apply(double input) {
+        return MathUtil.applyDeadband(input,deadBand);
+    }
 }
