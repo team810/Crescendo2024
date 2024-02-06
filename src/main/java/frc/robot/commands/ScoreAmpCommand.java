a57e2ea78c74115e8b97b1eeb6fe1cd5a7f757a2
Matthew Lomando <80913065+twinblasterrex@users.noreply.github.com>
2024-02-06 13:45:17 -0500

    Score Speaker Command and Score amp command written.

--- src/main/java/frc/robot/commands/ScoreAmpCommand.java
+++ src/main/java/frc/robot/commands/ScoreAmpCommand.java
@@ -1,34 +1,23 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystem.intake.IntakeStates;
+import frc.robot.subsystem.intake.IntakeSubsystem;
 
 
 public class ScoreAmpCommand extends Command {
 
     public ScoreAmpCommand() {
-        // each subsystem used by the command must be passed into the
-        // addRequirements() method (which takes a vararg of Subsystem)
-        addRequirements();
+
+        addRequirements(IntakeSubsystem.getInstance());
     }
 
     @Override
     public void initialize() {
-
+        IntakeSubsystem.getInstance().setState(IntakeStates.shoot);
     }
-
-    @Override
-    public void execute() {
-
-    }
-
-    @Override
-    public boolean isFinished() {
-        // TODO: Make this return true when this Command no longer needs to run execute()
-        return false;
-    }
-
     @Override
     public void end(boolean interrupted) {
-
+        IntakeSubsystem.getInstance().setState(IntakeStates.off);
     }
 }
